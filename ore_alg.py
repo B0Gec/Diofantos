5060e6a79c9be4ec168b77b66b09822ab5929617
bogec <bostogec@gmail.com>
2026-01-09 18:32:57 +0100

    experiments ended, results interesting

--- ore_alg.py
+++ ore_alg.py
@@ -4,19 +4,20 @@ import subprocess
 import re
 
 
-def oraj(data: list, execute_cmd=False, verbosity=0):
-    """Runs 5 lines of cocoa code with given points and returns the ideal
-        It makes sure the numper of variables is appropriate.
-        Cocoa code:
-            data = <given data>
-            guess(data, OreAlgebra(ZZ['n'], 'Sn'))
+def oraj(data: list, execute_cmd=False, is_berlekamp=False, verbosity=0):
+    """Runs equation guessing via ore_algebra or alternatively berlekamp_massey algorithm.
 
     Console command:
         miniforge3/envs/sage/bin/sage -c "from ore_algebra import *;data = [0, 1, 1,2,3, 5, 8];print(guess(data, OreAlgebra(ZZ['n'], 'Sn')))"
+            or (in case of using berlekamp_massey):
+        miniforge3/envs/sage/bin/sage -c "from sage.matrix.berlekamp_massey import berlekamp_massey;data = [0, 1, 1,2,3, 5, 8];berlekamp_massey(data)"
     """
 
     # limit data length
     data = data[:200]
+    if is_berlekamp:
+        if len(data) % 2 != 0:
+            data = data[:-1]
 
     CALL_SIZE_LIMIT=128000
     if len(f'{data}') > CALL_SIZE_LIMIT:
@@ -25,10 +26,15 @@ def oraj(data: list, execute_cmd=False, verbosity=0):
 
     sage_code = f"../miniforge3/envs/sage/bin/sage -c \"from ore_algebra import *;"
     sage_code += f"data = {data};print(guess(data, OreAlgebra(ZZ['n'], 'Sn')))\""
-
+    if is_berlekamp:
+        berlekamp_massey_code = "../miniforge3/envs/sage/bin/sage -c "
+        berlekamp_massey_code += "\"from sage.matrix.berlekamp_massey import berlekamp_massey;\n"
+        berlekamp_massey_code += f"data = {data};\npoly = berlekamp_massey(data);\nprint(poly)\""
 
     # b) execute cocoa file
     command = sage_code
+    if is_berlekamp:
+        command = berlekamp_massey_code
 
     if verbosity > 0:
         print(command)
@@ -40,8 +46,8 @@ def oraj(data: list, execute_cmd=False, verbosity=0):
         (output, err) = p.communicate()
         p_status = p.wait()
 
-        sage_res = output.decode()
-        return sage_res
+        bash_res = output.decode()
+        return bash_res
     print("NOT Executing LINUX command for real... just simulating command")
     raise ValueError('Not executing command for real!!')
 
@@ -54,22 +60,28 @@ if __name__ == "__main__":
     # ORAJ = True
     if ORAJ:
         # oraj([0, 1, 1, 2, 3, 5, 8])
-        ans = oraj([0, 1, 1, 2, 3, 5, 8], execute_cmd=True)
+        # ans = oraj([0, 1, 1, 2, 3, 5, 8], execute_cmd=True, is_berlekamp=True)
+        # ans = oraj([0, 1, 1, 2, 3, 5, 8], execute_cmd=False, is_berlekamp=True, verbosity=1)
+
+        ans = oraj([13424224, 1, 1, 2, 3, 5, 8,13, 1, 2], execute_cmd=True, is_berlekamp=True, verbosity=1)
         print(ans)
-        ans = oraj([0, 1, 1, 2], execute_cmd=True)
-        print(ans, len(ans))
-        # print(len(ans))
+        # ans = oraj([0, 1, 1, 2], execute_cmd=True)
+        # print(ans, len(ans))
+        # # print(len(ans))
 
     # results analysis
     ANALYZE = False
     ANALYZE = True
     if ANALYZE:
         import os
-        files = sorted(os.listdir('./results/ore_algebra0'))
+        # indir = 'ore_algebra0'
+        indir = 'berlk-messay0/444571'
+        indir = 'berlk-messay0/447940'
+        files = sorted(os.listdir(f'./results/{indir}'))
         print(files)
         for file in files[:160]:
             if file[-3:] == 'out':
-                with open(f"./results/ore_algebra0/{file}", 'r') as f:
+                with open(f"./results/{indir}/{file}", 'r') as f:
                     # print(f.read())
                     eq, is_disco = re.findall('eq = \'(.*)\'.+\nis Disco: (\w+)', f.read())[0]
                     if is_disco == 'True':
